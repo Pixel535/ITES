eb136ff0b735200347e70a2ff67372192e5c50ee
Mateusz <mati.mg01@wp.pl>
2026-03-09 23:32:48 +0100

    [FEATURE] New Maintenance

---
+++ functions/[[path]].js
@@ -0,0 +1,47 @@
+const SYSTEM_PAGE_PATHS = new Set([
+    '/maintenance',
+    '/maintenance.html',
+    '/error',
+    '/error.html'
+]);
+
+const STATIC_PREFIXES = [
+    '/assets/',
+    '/css/',
+    '/js/',
+    '/meta/',
+    '/data/'
+];
+
+function isStaticAsset(pathname) {
+    return STATIC_PREFIXES.some((prefix) => pathname.startsWith(prefix));
+}
+
+function isHtmlNavigation(request) {
+    if (request.method !== 'GET') return false;
+
+    const accept = request.headers.get('accept') || '';
+    return accept.includes('text/html');
+}
+
+function isMaintenanceEnabled(env) {
+    return String(env.MAINTENANCE_MODE || '').toLowerCase() === 'true';
+}
+
+export async function onRequest(context) {
+    const { request, env } = context;
+    const url = new URL(request.url);
+    const { pathname } = url;
+
+    if (
+        isMaintenanceEnabled(env) &&
+        isHtmlNavigation(request) &&
+        !SYSTEM_PAGE_PATHS.has(pathname) &&
+        !isStaticAsset(pathname)
+    ) {
+        url.pathname = '/maintenance.html';
+        return env.ASSETS.fetch(new Request(url.toString(), request));
+    }
+
+    return env.ASSETS.fetch(request);
+}
